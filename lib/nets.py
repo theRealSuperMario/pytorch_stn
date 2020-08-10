3fca02872b9300ef5eb09145930918c9a35dbdc1
Sandro Braun <supermario94123@gmail.com>
2020-08-10 11:54:52 +0200

    adds lib/nets.py

---
+++ lib/nets.py
@@ -0,0 +1,82 @@
+from __future__ import print_function
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torchvision
+from datasets import MNIST_TRANSLATED, StochasticPairs
+from torchvision import datasets, transforms
+import matplotlib.pyplot as plt
+import numpy as np
+from utils import *
+import cv2
+
+
+class LocalizationNet(nn.Module):
+    """theta matrix has 6 parameters"""
+
+    def __init__(self):
+        super(LocalizationNet, self).__init__()
+        self.localization = nn.Sequential(
+            nn.Conv2d(1, 8, kernel_size=7),
+            nn.MaxPool2d(2, stride=2),
+            nn.ReLU(True),
+            nn.Conv2d(8, 10, kernel_size=5),
+            nn.MaxPool2d(2, stride=2),
+            nn.ReLU(True),
+        )
+
+        self.classifier = nn.Sequential(
+            nn.Conv2d(1, 10, kernel_size=5),
+            nn.MaxPool2d(2),
+            nn.ReLU(),
+            nn.Conv2d(10, 20, kernel_size=5),
+            nn.Dropout2d(),
+            nn.MaxPool2d(2, stride=2),
+            nn.ReLU(True),
+            nn.Flatten(),  # 320
+            nn.Linear(320, 50),
+            nn.ReLU(),
+            nn.Dropout(),
+            nn.Linear(50, 10),
+        )
+
+        # Regressor for the 3 * 2 affine matrix
+        self.fc_loc = nn.Sequential(
+            nn.Linear(10 * 3 * 3, 32), nn.ReLU(True), nn.Linear(32, 3 * 2)
+        )
+
+        # Initialize the weights/bias with identity transformation
+        self.fc_loc[2].weight.data.zero_()
+        self.fc_loc[2].bias.data.copy_(
+            torch.tensor([1, 0, 0, 0, 1, 0], dtype=torch.float)
+        )
+
+    # Spatial transformer network forward function
+    def stn(self, x):
+        theta = self.stn_theta(x)
+
+        grid = F.affine_grid(theta, x.size())
+        x = F.grid_sample(x, grid)
+        return x
+
+    def stn_theta(self, x):
+        xs = self.localization(x)
+        xs = xs.view(-1, 10 * 3 * 3)
+        theta = self.fc_loc(xs)
+        theta = theta.view(-1, 2, 3)
+        return theta
+
+    def forward(self, x):
+        # transform the input
+        x = self.stn(x)
+
+        # Perform the usual forward pass
+        # x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        # x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        # x = x.view(-1, 320)
+        # x = F.relu(self.fc1(x))
+        # x = F.dropout(x, training=self.training)
+        # x = self.fc2(x)
+        x = self.classifier(x)
+        return F.log_softmax(x, dim=1)
